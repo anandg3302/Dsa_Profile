01aa31032416f859105db9f2a8dc96424f7c6cdc
anandg3302 <ag6072526@gmail.com>
2026-06-09 15:34:16 +0530

    Time: 2 ms (83.18%), Space: 46.4 MB (80.37%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -2,50 +2,75 @@ import java.util.*;
 
 class Solution {
 
-    List<List<String>> result = new ArrayList<>();
-    Set<Integer> col = new HashSet<>();
-    Set<Integer> diag = new HashSet<>();
-    Set<Integer> antidiag = new HashSet<>();
+    List<List<String>> ans = new ArrayList<>();
 
     public List<List<String>> solveNQueens(int n) {
-        char[][] board  = new char[n][n];
-        for(int i = 0; i < n; i++){
+
+        char[][] board = new char[n][n];
+
+        for(int i = 0; i < n; i++) {
             Arrays.fill(board[i], '.');
         }
-        backtrack(0, n, board);
-        return result;
+
+        backtrack(board, 0, n);
+
+        return ans;
     }
 
-    public void backtrack(int row, int n, char[][] board){
-        //  Base case 
-        if(row == n){
+    private void backtrack(char[][] board, int row, int n) {
+
+        if(row == n) {
+
             List<String> temp = new ArrayList<>();
-            for(char[] ch : board){
-                temp.add(new String(ch));
-            }
-            result.add(temp);
-            return; 
-        }
 
-        for(int cols = 0; cols < n; cols++){
-            if(col.contains(cols) || diag.contains(cols - row) || antidiag.contains(cols + row)){
-                continue;
+            for(char[] r : board) {
+                temp.add(new String(r));
             }
 
-            // placing queen
-            board[row][cols] = 'Q';
-            col.add(cols);
-            diag.add(cols - row);
-            antidiag.add(cols + row);
-            // recurse
-            backtrack(row + 1, n, board);
-
-            // backtrack
-            board[row][cols] = '.';
-            col.remove(cols);
-            diag.remove(cols - row);
-            antidiag.remove(cols + row);
+            ans.add(temp);
+            return;
         }
+
+        for(int col = 0; col < n; col++) {
+
+            if(isSafe(board, row, col, n)) {
+
+                board[row][col] = 'Q';
+
+                backtrack(board, row + 1, n);
+
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, int n) {
+
+        // column
+        for(int i = 0; i < row; i++) {
+            if(board[i][col] == 'Q')
+                return false;
+        }
+
+        // left diagonal
+        for(int i = row - 1, j = col - 1;
+            i >= 0 && j >= 0;
+            i--, j--) {
+
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        // right diagonal
+        for(int i = row - 1, j = col + 1;
+            i >= 0 && j < n;
+            i--, j++) {
+
+            if(board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
     }
 }
 
