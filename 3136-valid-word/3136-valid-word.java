565d0167abdb17e5e89f6316b5e6ab82032dd11a
anandg3302 <ag6072526@gmail.com>
2026-07-15 15:10:32 +0530

    Time: 1 ms (99.39%), Space: 43.4 MB (34.18%) - LeetHub

--- 3136-valid-word/3136-valid-word.java
+++ 3136-valid-word/3136-valid-word.java
@@ -2,13 +2,10 @@ class Solution {
     public boolean isValid(String word) {
         boolean vowel = false;
         boolean cons  = false;
-        boolean dig   = false;
         if (word.length() >= 3) {
             for (int i = 0; i < word.length(); i++) {
-               char c = word.charAt(i);
-               if(!Character.isLetterOrDigit(c)) return false;
+               if(!Character.isLetterOrDigit(word.charAt(i))) return false;
                if(Character.isDigit(word.charAt(i))){
-                dig = true;
                }
                if(Character.isLetter(word.charAt(i))){
                     char ch = word.charAt(i);
