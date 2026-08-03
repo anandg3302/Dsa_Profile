d19247acb384449f39c5970ca8e7c8b30920722a
anandg3302 <ag6072526@gmail.com>
2026-08-04 01:58:46 +0530

    Time: 4 ms (52.08%), Space: 46.1 MB (99.74%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,26 @@
 public class Solution {
 
-    	    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] nums, int target) {
 
-            	        Map<Integer, Integer> h = new HashMap<>();
+        Map<Integer, Integer> h = new HashMap<>();
 
-                        	        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
 
-                                    	            h.put(nums[i], i);
+            h.put(nums[i], i);
 
-                                                    	        }
-                                                                	        for (int i = 0; i < nums.length; i++) {
+        }
+        for (int i = 0; i < nums.length; i++) {
 
-                                                                            	            int y = target - nums[i];
+            int y = target - nums[i];
 
-                                                                                            	            if (h.containsKey(y) && h.get(y) != i) {
+            if (h.containsKey(y) && h.get(y) != i) {
 
-                                                                                                            	                return new int[] {i, h.get(y)};
+                return new int[] { i, h.get(y) };
 
-                                                                                                                                	            }
+            }
 
-                                                                                                                                                	        }
-                                                                                                                                                                          throw new IllegalArgumentException("No two sum solution");
-                                                                                                                                                                                  }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 
-                                                                                                                                                                                  	}
-
-                                                                                                                                                                                    
+}
