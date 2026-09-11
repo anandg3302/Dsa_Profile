3c6d3aa7fbd3515c2a3db91869aac7be6629796b
anandg3302 <ag6072526@gmail.com>
2026-09-11 18:56:21 +0530

    Time: 3 ms (64.11%), Space: 46.6 MB (46.87%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -3,12 +3,12 @@ class Solution {
 
         int[][] arr = new int[mat.length][2];
 
-        for(int i = 0; i < mat.length; i++) {
+        for (int i = 0; i < mat.length; i++) {
 
             int count = 0;
 
-            for(int j = 0; j < mat[0].length; j++) {
-                if(mat[i][j] != 0) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] != 0) {
                     count++;
                 }
             }
@@ -18,16 +18,16 @@ class Solution {
         }
 
         Arrays.sort(arr, (a, b) -> {
-            if(a[0] != b[0]) { 
+            if (a[0] != b[0]) {
                 return a[0] - b[0];
-             }
+            }
 
             return a[1] - b[1];
         });
 
         int[] ans = new int[k];
 
-        for(int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) {
             ans[i] = arr[i][1];
         }
 
