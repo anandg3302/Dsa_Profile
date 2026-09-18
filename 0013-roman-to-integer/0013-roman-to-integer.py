1d2c1eb3258ea9ed072944d8ae60d18a1683d302
anandg3302 <ag6072526@gmail.com>
2026-09-18 19:23:53 +0530

    Time: 6 ms (46.2%), Space: 19.4 MB (20.82%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        mp = {}
+        mp["I"] = 1
+        mp["V"] = 5
+        mp["X"] = 10
+        mp["L"] = 50
+        mp["C"] = 100
+        mp["D"] = 500
+        mp["M"] = 1000
+        sum = 0
+        for i in range(0,len(s)):
+            if i + 1 < len(s) and mp[s[i]] < mp[s[i+1]]:
+                sum -= mp[s[i]]
+            else:
+                sum += mp[s[i]]
+        
+        return sum
+            
